91fc72c8d6ffe05dcbfb7955a76a5b2ea9d79256
Niklas Maximilian Heinrich Horstmann <horstmann.business@gmail.com>
2025-08-06 10:28:14 +0000

    nun sehe ich 3 zeilen, schon besser. was hälst du davon, wenn du die spa

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -6,13 +6,11 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto">
-    <table
-      ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
-      {...props}
-    />
-  </div>
+  <table
+    ref={ref}
+    className={cn("w-full caption-bottom text-sm", className)}
+    {...props}
+  />
 ))
 Table.displayName = "Table"
 
@@ -115,3 +113,5 @@ export {
   TableCell,
   TableCaption,
 }
+
+    
