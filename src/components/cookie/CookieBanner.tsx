f55182c3fce4c07b0f6d80d04fb8b5d2f520b57e
CreateMyArt Administrator <example@example.com>
2025-08-26 21:01:11 +0200

    Cookie-Banner: Dünnen weißen Strich entfernt

--- src/components/cookie/CookieBanner.tsx
+++ src/components/cookie/CookieBanner.tsx
@@ -36,7 +36,7 @@ export function CookieBanner() {
   };
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-50 p-4 border-t border-gray-200 shadow-lg">
+    <div className="fixed bottom-0 left-0 right-0 z-50 p-4 shadow-lg">
       <Card className="max-w-4xl mx-auto">
         <CardHeader className="pb-3">
           <div className="flex items-center justify-between">
