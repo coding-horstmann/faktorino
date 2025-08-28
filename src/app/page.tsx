45f953e2ea9d154d6427474f7a4e56d38a942be2
coding-horstmann <coding.horstmann@gmail.com>
2025-08-28 17:11:08 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -240,7 +240,7 @@ export default function Home() {
                          </AccordionContent>
                      </AccordionItem>
                       <AccordionItem value="item-9">
-                         <AccordionTrigger className="text-lg">Muss ich die erstellten Rechnungen testen??</AccordionTrigger>
+                         <AccordionTrigger className="text-lg">Muss ich die erstellten Rechnungen testen?</AccordionTrigger>
                          <AccordionContent className="text-base">
                              Ja, faktorio dient ausschließlich der Unterstützung bei der Rechnungserstellung.
                              Es ersetzt keine steuerliche oder rechtliche Beratung.
@@ -248,7 +248,7 @@ export default function Home() {
                          </AccordionContent>
                      </AccordionItem>
                       <AccordionItem value="item-10">
-                         <AccordionTrigger className="text-lg">Unterstützt das Tool das OSS-Verfahren???</AccordionTrigger>
+                         <AccordionTrigger className="text-lg">Unterstützt das Tool das OSS-Verfahren?</AccordionTrigger>
                          <AccordionContent className="text-base">
                              Nein. faktorino unterstützt das One-Stop-Shop Verfahren nicht.
                          </AccordionContent>
